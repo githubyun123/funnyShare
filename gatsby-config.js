5e596e825d320b35017f10d2f8151517aeaad581
wy <1144571250@qq.com>
2022-09-10 14:32:27 +0800

    runningMessageZhou

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: '周观云的跑步记录',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://github.com/githubyun123',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
